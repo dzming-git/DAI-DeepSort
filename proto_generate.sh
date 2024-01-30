9337d346463d074e900da3c09e5560086db346c0
dzming <dzm_work@163.com>
2024-01-30 02:25:14 +0000

    添加grpc proto文件与代码生成脚本

---
+++ proto_generate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+protos=(
+    image_harmony
+    service_coordinator
+    target_detection
+    target_tracking
+)
+
+for proto in "${protos[@]}"
+do
+    mkdir -p ./generated/protos/$proto/
+    cp ./resources/protos/$proto.proto ./generated/protos/$proto/
+    python3 -m grpc_tools.protoc -I . --python_out=. --grpc_python_out=. ./generated/protos/$proto/$proto.proto
+    rm ./generated/protos/$proto/$proto.proto
+done
