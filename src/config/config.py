4616e1059ba2c13d2bd9f966e6a2a863049398d5
dzming <dzm_work@163.com>
2024-01-31 12:19:37 +0000

    删除config读取的多余代码

--- src/config/config.py
+++ src/config/config.py
@@ -33,29 +33,6 @@ class Config:
         self.service_name = service_data.get('name', '')
         self.service_port = str(service_data.get('port', ''))
         self.service_tags = service_data.get('tags', [])
-        weight_info_folder = service_data.get('weight_info_folder', '')
-        weight_info_paths = []
-        
-        if weight_info_folder:
-            for root, dirs, files in os.walk(weight_info_folder):
-                file_name: str
-                for file_name in files:
-                    if file_name.endswith('.yml') or file_name.endswith('yaml'):
-                        file_path = os.path.join(root, file_name)
-                        weight_info_paths.append(file_path)
-        for weight_info_path in weight_info_paths:
-            with open(weight_info_path, 'r') as f:
-                filename = os.path.basename(weight_info_path)
-                weight_info_data = yaml.safe_load(f)
-                weight_info = WeightInfo()
-                weight_info.file = os.path.splitext(filename)[0]
-                data_path = weight_info_data['data_path']
-                self.weights_data_map[weight_info.file] = data_path
-                with open(data_path, 'r') as f_data:
-                    data_data = yaml.safe_load(f_data)
-                    weight_info.labels = list(data_data['names'].values())
-                self.weights_map[weight_info.file] = weight_info
-        
         consul_data = config_data.get('consul', {})
         self.consul_ip = consul_data.get('ip', '')
         self.consul_port = consul_data.get('port', '')
