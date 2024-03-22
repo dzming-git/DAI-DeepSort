96d55ee065221904de3e627c234c25e5ead6b448
dzming <dzm_work@163.com>
2024-03-22 02:02:25 +0000

    port数据类型修改为int

--- src/config/config.py
+++ src/config/config.py
@@ -13,13 +13,13 @@ class Config:
     def __init__(self):
         # service
         self.service_name: str = ''
-        self.service_port: str = ''
+        self.service_port: int = -1
         self.service_tags: List[str] = list()
         self.weights_folder: str = ''
 
         #consul
-        self.consul_ip: str
-        self.consul_port: str
+        self.consul_ip: str = ''
+        self.consul_port: int = -1
         
         self.load_config()
 
@@ -29,9 +29,9 @@ class Config:
 
         service_data = config_data.get('service', {})
         self.service_name = service_data.get('name', '')
-        self.service_port = str(service_data.get('port', ''))
+        self.service_port = service_data.get('port', -1)
         self.service_tags = service_data.get('tags', [])
         self.weights_folder = service_data.get('weights_folder', './weights')
         consul_data = config_data.get('consul', {})
         self.consul_ip = consul_data.get('ip', '')
-        self.consul_port = consul_data.get('port', '')
+        self.consul_port = consul_data.get('port', -1)
